af96e0a0088e6470900340364510cec1eb0834b9
Felo <felo88trejosc@gmail.com>
2023-08-11 15:26:54 -0600

    loginysignup

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -21,9 +21,9 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registrarse/', views.registrarse, name='registrarse'),
-    path('acceso/', views.acceso, name='acceso'),
-    path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_ sesion'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('sign_out/', views.sign_out, name='sign_out'),
     path('home/', views.home, name='home'),
 
 ]
